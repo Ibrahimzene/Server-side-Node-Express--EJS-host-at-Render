5313c6a0ed247fcc2e20308348ce9d0b4d85d65a
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-17 11:30:06 +0100

    Add getPort function and update server configuration

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
 export const getPoet = () => {
     return 1724;
 }
+
+export function getPort(): any {
+	throw new Error('Function not implemented.');
+}
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,18 @@
-import express from "express";
-import path from "path";
-import * as config from "./config";
+import express from 'express';
+import * as config from './config';
+import path from 'path';
+import * as model from './model';
 
 const app = express();
 const baseDir = process.cwd();
 
 app.set('view engine', 'ejs');
-app.set('views', `$baseDir/src/views`);
+app.set('views', path.join(baseDir, '/src/views'));
 
-app.get("/", (req, res) => {
-	const siteTitle = "Node Express Site with EJS Hosted on Render";
-	res.send(`
-<html>
-	<head>
-		<title>${siteTitle}</title>
-		<link rel="stylesheet" href="/css/main.css">
-	</head>
-	<body>
-		<h1>${siteTitle}</h1>
-		<p>Declare the server-side build process for a Node Express site by incorporating EJS, and specify the hosting platform as Render. This involves setting up the necessary configurations, dependencies, and deploying the application to Render for public access.</p>
-	</body>
-</html>
-	`);
+app.get('/', (req, res) => {
+	res.render('pages/welcome', {title: 'The Welcome Page', books: model.getBooks()});
 });
 
-app.get("/css/main.css", (req, res) => {
-	res.send(`
-body {
-	color: #545454;
-}
-	`);
-});
-
-app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}`);
+app.listen(config.getPort(), () => {
+	console.log(`Listening at http://localhost:${config.getPort()}`);
 });
